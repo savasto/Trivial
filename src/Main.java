1f9d3e69c7928febae95601e12a2c72a515da437
savasto <savastova.olga@gmail.com>
2022-02-15 11:25:53 +0100

    commit showsQuestions and answerQuestions

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,28 @@ public class Main {
 	List<Question> questions = new ArrayList<>();
         questions =  createQuestion(questions);
         System.out.println("Lista de preguntas" );
+        showQuestions(questions);
+        int contCorrect = answerQuestions(questions);
+    }
+
+    private static int answerQuestions(List<Question> questions) {
+        int contCorrect=0;
+        for (Question currentQuestion:questions) {
+            if(currentQuestion.isTrue()==writeAnswer()){
+                contCorrect++;
+            }
+        }
+        return contCorrect;
+    }
+
+    private static boolean writeAnswer() {
+        return (new Scanner(System.in).nextLine()).equalsIgnoreCase("S");
+    }
+
+    private static void showQuestions(List<Question> questions) {
+        for (Question currentQuestion:questions) {
+            System.out.println(currentQuestion);
+        }
     }
 
     private static List<Question> createQuestion(List<Question>questions) {
