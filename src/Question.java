86537df471fc8e263c36cacf05f6d20d16d293d0
Aleix Alvarez <anfifus@gmail.com>
2022-02-15 10:45:43 +0100

    Initial commit

---
+++ src/Question.java
@@ -0,0 +1,35 @@
+public class Question {
+    private String question;
+    private boolean isTrue;
+
+    public Question(String question, boolean isTrue) {
+        this.question = question;
+        this.isTrue = isTrue;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "question='" + question + '\'' +
+                ", isTrue=" + isTrue +
+                '}';
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public boolean isTrue() {
+        return isTrue;
+    }
+
+    public void setTrue(boolean aTrue) {
+        isTrue = aTrue;
+    }
+
+
+}
